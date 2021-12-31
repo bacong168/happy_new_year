8ab8ba92f8f660c92964d246f08a93e08628fc66
congnb168 <nguyenbacong168@gmail.com>
2021-12-31 22:40:40 +0700

    pushcode

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://fonts.googleapis.com/css?family=Cookie|Dancing+Script|Great+Vibes|Lobster+Two|Monoton|Sacramento" rel="stylesheet">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<!-- // <script type="text/javascript" src="firework.js"></script> -->
+	<script type="text/javascript" src="app.js"></script>
+	<script type="text/javascript" src="particle_sets.js"></script>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+
+</head>
+<body>
+<div id="box">
+<center><p>Happy New Year 2022<br>😊🎇🎆🎉🎊🎊YÊU EM</p></center>
+<br>
+<center><p class="l">“I wish that all your wishes are fulfilled this year… Happy new year 🎊<BR><BR>
+
+Điều may mắn trong 2021 anh có em và cảm ơn em vì tất cả. Yêu em nhiều ❤<BR><BR>
+
+“Năm mới chúc người yêu của anh thật xinh đẹp giỏi giang và yêu anh hơn nữa nha”.<BR>
+   <img src="img.png" style="width: 160px; height: 220px;margin-top: 20px;">
+</p></center>
+</div>
+<canvas id="canvas"></canvas>
+</body>
+</html>
